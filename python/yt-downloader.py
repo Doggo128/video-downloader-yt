a273747b587f0b0d851e7b5c764f73b364d2f459
Doggo128 <88943366+Doggo128@users.noreply.github.com>
2021-11-18 20:41:08 -0600

    GUI changes + basics
    changed GUI elements, some error checking.

--- python/yt-downloader.py
+++ python/yt-downloader.py
@@ -1,5 +1,7 @@
+from logging import DEBUG
 from tkinter import *
 import tkinter
+from tkinter.font import Font
 from typing import List
 from pytube import YouTube
 
@@ -8,10 +10,17 @@ from tkinter.filedialog import asksaveasfilename
 from tkinter.filedialog import askopenfilename
 
 
+DEBUG_VERSION = "v0.0"
+DEBUG_PRINTERRORS = True
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 530
+WINDOW_STARTX = 200
+WINDOW_STARTY = 200
 
 window = Tk() 
-window.title("Youtube Downloader v0")
-window.geometry("600x500+500+500") #Window dimensions (width x height + x + y)
+window.title("Youtube Downloader " + DEBUG_VERSION)
+
+window.geometry( str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT) + "+" + str(WINDOW_STARTX) + "+" + str(WINDOW_STARTY) ) #Window dimensions (width x height + x + y)
 window.resizable( width=False, height=False ) #This disables resizing the window
 
 #filters user input for the youtube link formatting
@@ -19,38 +28,32 @@ def filterInput(str=""):
     #If string is empty
     if len(str) <= 0:
         return None
+    
+
+    #TODO: add checks for the youtube short urls (youtu.be links)
+    if "youtube.com" not in str.lower():
+        if DEBUG_PRINTERRORS == True:
+            print( "youtube.com not in str" )
+        return None
    
     return str
 
 
 
-lblTitle = Label(window, text="Enter Link")
+lblTitle = Label(window, text="Enter Link", font="Calibri")
 
+lblVersion = Label(window, text=DEBUG_VERSION, font="Calibri")
 
 inputBoxUrl = Entry( window, width=64)
-#inputBoxUrl.place( x=100, y=100)
-
-lblStreams = Label(window, text='Streams')
-
-
-#The TreeView that displays the video data
-treeVD = ttk.Treeview(window, height=2)
-treeVD['columns']=('Title', 'Views', 'Description')
-
-treeVD.column( '#0', width=0, anchor=CENTER, stretch=NO)
-treeVD.column('Title',  width=80, anchor='w')
-treeVD.column('Views',  width=80, anchor='w')
-treeVD.column('Description', width=250, anchor='w')
-
-treeVD.heading( '#0', text='', anchor=CENTER)
-treeVD.heading( 'Title', text='Title', anchor=CENTER)
-treeVD.heading( 'Views', text='Views', anchor=CENTER)
-treeVD.heading( 'Description', text='Description', anchor=CENTER)
 
 
 
 
-treeITag = ttk.Treeview(window, height=8)
+lblFrameItag = LabelFrame(window, text="Streams", width=500, height=250)
+
+
+
+treeITag = ttk.Treeview(lblFrameItag, height=8)
 treeITag['columns'] = ( 'itag', 'type', 'vcodec', 'acodec', 'resolution', 'fps')
 
 treeITag.column( '#0', width=0, anchor=CENTER, stretch=NO)
@@ -71,6 +74,34 @@ treeITag.heading( 'resolution', text='Resolution', anchor=CENTER)
 treeITag.heading( 'fps', text='FPS', anchor=CENTER)
 
 
+lblFrame = LabelFrame(window, text="Video Information", width=400, height=150)
+
+lblVidName = Label( lblFrame, text="Title: ", font="Calibri")
+lblVidViews = Label( lblFrame, text="Views:", font="Calibri" )
+lblVidDesc = Label( lblFrame, text="Description:\n", wraplength=300, justify="left", font="Calibri" )
+
+
+lblHelp = Label(window, text="Help", fg="blue", cursor="hand2")
+
+windowHelp = None
+
+
+def clickedHelpMenu(self):
+    #if window is already existing then exit
+    windowHelp = Toplevel(window, width=256, height=400)
+    windowHelp.title("Help")
+    windowHelp.resizable( width=False, height=False )
+
+    lblHeader1 = Label( windowHelp, text="Insert YouTube link @ 'Enter Link'.\nThen click the 'Fetch Video' button.\nOnce video details are grabbed, select your desired Stream.\nThen click the 'Download Stream' button. \nYou will be asked to pick a location to save the stream as a video file.", wraplength=200, justify="left", width=40, height=16)
+    lblHeader1.place( x=0, y=0)
+
+    lblHeader2 = Label( windowHelp, text="lorem ipsum", width=20, height=2)
+    lblHeader1.place( x=10, y=5)
+
+    return None
+
+lblHelp.bind("<Button-1>", clickedHelpMenu)
+
 
 selectedITag = 0
 
@@ -78,12 +109,7 @@ def itagSelected(self):
     curItem = treeITag.focus()
     print( treeITag.item(curItem) )
     x = list(treeITag.item(curItem).values() )
-    #print( x[2][0] )
     
-
-    #print( "ITag: ", x[2][0] )
-    #print( "Type: ", x[2][1] )
-    #print( "vcodec: %s", x[2][2] )
 
     global selectedITag
     selectedITag = x[2][0]
@@ -138,7 +164,11 @@ def getVideo():
     print("Views: ", yt.views )
     print( "Description: ",yt.description)
 
-    treeVD.insert( parent='', index=0, iid=0, text='', values=( yt.title, str(yt.views), yt.description) )
+    lblVidName.config( text="Title: " + yt.title)
+    lblVidViews.config( text="Views: " + str(yt.views) )
+    lblVidDesc.config( text="Description:\n" + yt.description)
+
+    #treeVD.insert( parent='', index=0, iid=0, text='', values=( yt.title, str(yt.views), yt.description) )
 
     fetchStreams()
 
@@ -176,32 +206,39 @@ def downloadVideo():
     print("File Name: ", fileName)
    
     ys.download( output_path=filePath, filename=fileName) 
+    tkinter.messagebox.showerror( title="Complete!",  message="Video Download Finished")
     print("DONE")
     print("---------------------")
 
 
 
-btnDownload = Button( window, text="Download Stream", width=16, height=1, command=downloadVideo, state=DISABLED)
+btnDownload = Button( lblFrameItag, text="Download Stream", width=16, height=1, command=downloadVideo, state=DISABLED)
 
 btnGo = Button( window, text="Fetch Video", width=12, height=1, command=getVideo )
 
 
-lblTitle.place( x=300, y=0 )
+lblTitle.place( x=90, y=4 )
 
 
 #width x height = 600 x 400 (urlbox is 64width)
 inputBoxUrl.place( x=( 80  ), y=30 )
 btnGo.place( x=480, y=30 )
 
-
-treeVD.place( x=80, y=70)
-
-lblStreams.place( x=300, y=180)
-treeITag.place( x=80, y=200)
-
-btnDownload.place( x=250, y=400)
+#Video Information 
+lblFrame.place( x=80, y=70 )
+lblVidName.place( x=0, y=0)
+lblVidViews.place( x=0, y=20)
+lblVidDesc.place( x=0, y=40)
 
 
+
+lblFrameItag.place( x=70, y=240)
+treeITag.place( x=25, y=0) #x=80, y=250
+
+btnDownload.place( x=300, y=195) #x=250, y=450
+lblVersion.place( x=WINDOW_WIDTH - 40, y=WINDOW_HEIGHT - 30 )
+
+lblHelp.place( x=WINDOW_WIDTH-80, y=WINDOW_HEIGHT - 30)
 
 
 #print( type(widgets[0] ) )
